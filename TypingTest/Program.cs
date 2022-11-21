add64c63b33d7105de726942c482a266c9b00f12
Николай Чернявский <75169213+ChernNic@users.noreply.github.com>
2022-11-21 22:06:12 +0300

    first commit

---
+++ TypingTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TypingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypingSpeedTest typingSpeedTest = new TypingSpeedTest();
+            typingSpeedTest.Test();
+
+        }
+    }
+}
